589cc69b2a8c160dd18b89ed88f5a62cc3e5f7da
zeus-dev919 <cccodechef@gmail.com>
2021-11-29 08:20:15 +0530

    Db migration for django admin with new db

--- medico/medico/settings.py
+++ medico/medico/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'medico.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'medi',
+	'USER': 'medipocket',
+	'PASSWORD':'medipocket21',
+	'HOST':'localhost',
+	'PORT':'',
     }
 }
 
