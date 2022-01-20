bdc8e3e75f948ff43e0cf31833c1134097476c2d
zeus-dev919 <cccodechef@gmail.com>
2022-01-21 02:18:09 +0530

    specialization name added

--- medico/authentication/models.py
+++ medico/authentication/models.py
@@ -13,7 +13,7 @@ class Specialization(models.Model):
     hospital_name = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
-        return self.first_name
+        return self.specialization_name
 
 
 class Doctor(models.Model):
