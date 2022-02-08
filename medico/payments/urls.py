1ca96fe62c9509a115556a33fe2f6419635193db
zeus-dev919 <cccodechef@gmail.com>
2022-02-08 23:52:16 +0530

    final test changes

--- medico/payments/urls.py
+++ medico/payments/urls.py
@@ -4,5 +4,6 @@ from payments import views
 
 
 urlpatterns = [
-    url(r'^test-payment/$', views.test_payment),
+    # url(r'^test-payment/$', views.test_payment),
+    url(r'^save-stripe-info/$', views.save_stripe_info),
 ]
--- medico/payments/views.py
+++ medico/payments/views.py
@@ -8,14 +8,23 @@ from django.http  import HttpResponse
 stripe.api_key = 'sk_test_NabZ97T2L9XSSUZapustQMXj'
 
 
-# @api_view(['POST'])
+@api_view(['POST'])
+def save_stripe_info(request):
+  data = request.data
+  email = data['email']
+  payment_method_id = data['payment_method_id']
+  extra_msg = 'Your Payment has been successful' # add new variable to response message  # checking if customer with provided email already exists
+  customer_data = stripe.Customer.list(email=email).data
 
-def test_payment(request):
-
-    test_payment_intent = stripe.PaymentIntent.create(
-    amount=1000, currency='pln',
-    payment_method_types=['card'],
-    receipt_email='samryker@gmail.com')
-
-    # return Response(status=status.HTTP_200_OK, data=test_payment_intent)
-    return HttpResponse(test_payment_intent)
+  # if the array is empty it means the email has not been used yet
+  if len(customer_data) == 0:
+    # creating customer
+    customer = stripe.Customer.create(
+    email=email, payment_method=payment_method_id)
+  else:
+    customer = customer_data[0]
+    extra_msg = "Customer already existed."
+  return Response(status=status.HTTP_200_OK,
+    data={'message': 'Success', 'data': {
+      'customer_id': customer.id, 'extra_msg': extra_msg}
+   })
