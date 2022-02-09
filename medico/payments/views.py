c10607ed44b1030a55cd480f9e20804af02a2cba
zeus-dev919 <cccodechef@gmail.com>
2022-02-10 03:05:47 +0530

    added payment intent

--- medico/payments/views.py
+++ medico/payments/views.py
@@ -12,6 +12,7 @@ stripe.api_key = 'sk_test_NabZ97T2L9XSSUZapustQMXj'
 def save_stripe_info(request):
   data = request.data
   email = data['email']
+
   payment_method_id = data['payment_method_id']
   extra_msg = 'Your Payment has been successful' # add new variable to response message  # checking if customer with provided email already exists
   customer_data = stripe.Customer.list(email=email).data
@@ -24,6 +25,12 @@ def save_stripe_info(request):
   else:
     customer = customer_data[0]
     extra_msg = "Customer already existed."
+    stripe.PaymentIntent.create(
+    customer=customer,
+    payment_method=payment_method_id,
+    currency=data['currency'], # you can provide any currency you want
+    amount=data['amount'])
+
   return Response(status=status.HTTP_200_OK,
     data={'message': 'Success', 'data': {
       'customer_id': customer.id, 'extra_msg': extra_msg}
