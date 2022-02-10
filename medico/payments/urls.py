a41198528d016f1cda7b51dfec2812b44c3e5666
zeus-dev919 <cccodechef@gmail.com>
2022-02-11 01:43:21 +0530

    urls.py change for payments

--- medico/payments/urls.py
+++ medico/payments/urls.py
@@ -4,6 +4,9 @@ from payments import views
 
 
 urlpatterns = [
-    # url(r'^test-payment/$', views.test_payment),
+    url(r'^test-payment/$', views.test_payment),
     url(r'^save-stripe-info/$', views.save_stripe_info),
+    url(r'^confirm-payment-intent/$', views.confirm_payment_intent),
+
+
 ]
