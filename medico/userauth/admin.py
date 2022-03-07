2230f4ba089c9daab3a249ce7e84dbb050de0ccf
zeus-dev919 <cccodechef@gmail.com>
2022-03-08 02:30:58 +0530

    no message

--- medico/userauth/admin.py
+++ medico/userauth/admin.py
@@ -3,11 +3,9 @@ from .models import ExtendUser, CapturePost
 from django.apps import apps 
 # Register your models here.
 admin.site.register(ExtendUser)
+admin.site.register(CapturePost)
 
 
 app = apps.get_app_config('graphql_auth')
 
 
-
-for model_name, model in app.models.items():
-    admin.site.register(model)
